3efe4fb96746c2bd8e283220ae5200a8daabdb61
Martin Guest <martinguest@Martins-MacBook-Pro.local>
2020-03-16 17:41:56 +0000

    Add parameters

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -27,10 +28,11 @@ public class DemoController {
         return "hello";
     }
 
-    // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=Tim&age=10
     @GetMapping("welcome")
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Tim"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
         // prefix + name + suffix
         // /WEB-INF/view/welcome.jsp
