6b087328aa2a44e9b13ab00a48880899d9f76126
Martin Guest <martinguest@Martins-MacBook-Pro.local>
2020-03-16 17:26:31 +0000

    Add service and clean up model

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming.controller;
 
+import academy.learnprogramming.service.DemoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @Controller
 public class DemoController {
+
+    public final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
 
     // http://localhost:8080/todo-list/hello
     @ResponseBody
@@ -21,16 +30,20 @@ public class DemoController {
     // http://localhost:8080/todo-list/welcome
     @GetMapping("welcome")
     public String welcome(Model model) {
-        model.addAttribute("user", "Tim");
-        log.info("model = {}", model);
         // prefix + name + suffix
         // /WEB-INF/view/welcome.jsp
         return "welcome";
     }
 
+    @ModelAttribute("helloMessage")
+    public String helloMessage() {
+        log.info("helloMessage() called");
+        return demoService.getHelloMessage("Tim");
+    }
+
     @ModelAttribute("welcomeMessage")
     public String welcomeMessage() {
         log.info("welcomeMessage() called");
-        return "Welcome to this Demo application";
+        return demoService.getWelcomeMessage();
     }
 }
