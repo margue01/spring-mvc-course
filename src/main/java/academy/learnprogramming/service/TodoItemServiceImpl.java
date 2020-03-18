d53d6a1364ac633a26436c74e8010c44dd8498cc
Martin <guest.martin@gmail.com>
2020-03-18 10:56:48 +0000

    Add todo Items MVC
    - View Todo item
    - Add item with Post Redirect Get

---
+++ src/main/java/academy/learnprogramming/service/TodoItemServiceImpl.java
@@ -0,0 +1,33 @@
+package academy.learnprogramming.service;
+
+import academy.learnprogramming.model.TodoData;
+import academy.learnprogramming.model.TodoItem;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemServiceImpl implements TodoItemService {
+
+    @Getter
+    private final TodoData data = new TodoData();
+
+    @Override
+    public void addItem(TodoItem item) {
+        data.addItem(item);
+    }
+
+    @Override
+    public void removeItem(int id) {
+        data.removeItem(id);
+    }
+
+    @Override
+    public TodoItem getItem(int id) {
+        return data.getItem(id);
+    }
+
+    @Override
+    public void updateItem(TodoItem item) {
+        data.updateItem(item);
+    }
+}
